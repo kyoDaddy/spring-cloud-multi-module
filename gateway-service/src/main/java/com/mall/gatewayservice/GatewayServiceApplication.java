4a9b60ee1f536719ab299f1cb75793d543aaed5c
KYO <rlarbghrbgh@gmail.com>
2021-09-07 00:03:37 +0900

    1. spring-cloud-config 사용하여 config-service 모듈 추가 및 구성 2. user-service, item-service, order-service 에 spring-cloud-config 바로보도록 설정 3. 모든 모듈 개별 banner 적용

--- gateway-service/src/main/java/com/mall/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/mall/gatewayservice/GatewayServiceApplication.java
@@ -1,6 +1,8 @@
 package com.mall.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +13,18 @@ public class GatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+    /* spring actuator httpTrace
+          - http를 trace한 정보를 노출(기본적으로 최신 100건의 reqest-response를 보여줌)
+
+          - actuator는 상당히 민감한 정보를 많이 볼 수 있어서, 운영시 보안에 신경 써여함.. 실사용시에 Role을 설정하여 특정 사용자만 접근하도록 변경 필요
+          - 정보가 메모리에 저장되어, 운영시에는 영구저장소에 저장되도록 변경 필요
+          
+     */
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
     }
 
 }
--- user-service/src/main/java/com/mall/userservice/config/database/H2Console.java
+++ user-service/src/main/java/com/mall/userservice/config/database/H2Console.java
@@ -2,7 +2,12 @@ package com.mall.userservice.config.database;
 
 import lombok.extern.slf4j.Slf4j;
 import org.h2.tools.Server;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
@@ -13,25 +18,27 @@ import java.sql.SQLException;
 @Slf4j
 @Component
 @Profile("local")
-public class H2Console {
+public class H2Console implements InitializingBean, DisposableBean {
 
     private Server webServer;
-    private Server tcpServer;
+    private final String webPort = "8082";
 
-    private String webPort = "8078", tcpPort = "9092";
-
-    @EventListener(ContextRefreshedEvent.class)
-    public void start() throws SQLException {
-        log.info("starting h2 console at port -> {}, tcp port -> {}", webPort, tcpPort);
+    /**
+     * Bean 생성 시 동작할 코드 구현
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.info("starting h2 console at port -> {}", webPort);
         this.webServer = Server.createWebServer("-webPort", webPort, "-tcpAllowOthers").start();
-        this.tcpServer = Server.createTcpServer("-tcpPort", tcpPort, "-tcpAllowOthers").start();
     }
 
-    @EventListener(ContextClosedEvent.class)
-    public void stop() {
-        log.info("stopping h2 console at port -> {}, tcp port -> {}", webPort, tcpPort);
+    /**
+     * Bean 소멸 시 동작할 코드 구현
+     */
+    @Override
+    public void destroy() throws Exception {
+        log.info("stopping h2 console");
         this.webServer.stop();
-        this.tcpServer.stop();
-    }
 
+    }
 }
